ea0241a46a30a1dd463ccfb19c82961a576038c2
mohammedzubairuddinzameer <mohammedzubairuddinzameer@gmail.com>
2026-06-16 23:40:47 +0530

    Add animation for blob elements using GSAP

---
+++ js/blob.js
@@ -0,0 +1,36 @@
+// ==============================
+// blob.js
+// ==============================
+
+gsap.to(".blob1", {
+
+    x: 120,
+    y: 60,
+    duration: 12,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+
+});
+
+gsap.to(".blob2", {
+
+    x: -100,
+    y: 80,
+    duration: 15,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+
+});
+
+gsap.to(".blob3", {
+
+    x: 90,
+    y: -60,
+    duration: 18,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+
+});
